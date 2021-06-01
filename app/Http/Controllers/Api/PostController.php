832e1dfcac63d9fe51b37b653c9235004e082bc1
rendiputra <rendiputrapradana@gmail.com>
2021-06-01 22:13:34 +0700

    make controller && instal nuxtjs

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new PostResource(Post::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //set validation
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        // response saat tidak lulus validation
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400); //membawa pesan error dan status code error 400
+        }
+
+        //save to database
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        return new PostResource($post);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //set validation
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        // response saat tidak lulus validation
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400); //membawa pesan error dan status code error 400
+        }
+
+        //update error validation
+        $post->update([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        return new PostResource($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post->delete();
+
+        return new PostResource($post);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Route resource 'Posts'
+ */
+
+Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
