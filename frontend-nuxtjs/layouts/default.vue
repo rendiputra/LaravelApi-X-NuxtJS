f3d49a421ad278877a7f3420253342288f7f615a
rendiputra <rendiputrapradana@gmail.com>
2021-06-02 06:09:09 +0700

    mengedit layouts/default.vue && nuxt.config.js

--- frontend-nuxtjs/layouts/default.vue
+++ frontend-nuxtjs/layouts/default.vue
@@ -1,62 +1,31 @@
 <template>
   <div>
-    <Nuxt />
+
+    <div>
+      <b-navbar toggleable="lg" type="light" variant="light" class="shadow-md" fixed="top">
+        <b-navbar-brand to="/">Belajar NUXTJS</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/post">POST</b-nav-item>
+            <b-nav-item href="https://github.com/rendiputra">github</b-nav-item>
+            <b-nav-item href="https://github.com/rendiputra/LaravelApi-X-NuxtJS">Source code lengkap</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+
+    <Nuxt/>
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<style scoped>
+body{
+  background-color: lightgray;
 }
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.mt-5 {
+  margin-top: 100px!important;
 }
 </style>
--- frontend-nuxtjs/nuxt.config.js
+++ frontend-nuxtjs/nuxt.config.js
@@ -39,7 +39,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: 'http://localhost:8000'
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
